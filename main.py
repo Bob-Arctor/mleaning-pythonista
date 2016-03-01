fixed logreg, some test datafiles added need to add stohastic gradient descent

main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from mlearning import *
 from tools import *
 
-'''
+
 plt.close()
 samples = 50*np.random.rand(1000,1)
 target = 50.45 - 41.7 * samples
@@ -29,31 +29,50 @@ estimation = np.dot(samples_n, weights)
 plt.plot(samples, estimation, 'y')
 plt.plot(samples, target, 'g')
 plt.show()
-'''
 
+'''
 #samples = 50*np.random.rand(18,5)
 #samples = np.array([[1,2,3],[2,2,2],[4,4,4],[6,4,1]])
 #print(samples)
 print('-'*20)
 plt.close()
 #loading data
-data = np.genfromtxt('logreg.txt', dtype=None, delimiter='/t', skip_header=1)
-data = [x.split('   ') for x in data]
-data = [ [s.strip() for s in x] for x in data]
-data = [ [np.nan if s=='.' else float(s) for s in x] for x in data]
-data = np.array(data)
-data = delnans(data)
-target = np.atleast_2d(data[:,3])
-samples = np.concatenate([data[:,:3],data[:,4:]],axis=1)
-samples_t = samples[:100,:]
-samples_p = samples[100:,:]
-target_t = target.T[:100,:]
-target_p = target.T[100:,:]
-print(target_t.shape)
-print(samples_t.shape)
-#print(target.T)
-weights = logreg(samples_t, target_t, iterations=50, rate=0.000001)
-print(weights)
+
+data = np.genfromtxt('logreg2.txt', dtype=str, delimiter='\n',skip_header=1)
+#print(data)
+data = [x.split('\t') for x in data]
+#data = [ [s.strip() for s in x] for x in data]
+#data = [ [np.nan if s=='.' else float(s) for s in x] for x in data]
+data = np.asfarray(data)
+#data = delnans(data)
+
+target = np.atleast_2d(data[:,2]).T
+#samples = np.concatenate([data[:,:3],data[:,4:]],axis=1)
+samples = data[:,:2]
+print(samples.shape)
+print(target.shape)
+#samples_t = samples[:100,:]
+#s_t_n = rescale(samples_t)
+#s_p_n = rescale(samples_p)
+#samples_p = samples[100:,:]
+#target_t = target.T[:100,:]
+#target_p = target.T[100:,:]
+#print(samples_t.shape)
+
+weights = logreg(samples, target, iterations=7000, rate=0.001)
+#print(weights)
 plt.show()
-print(np.dot(add_ones(samples_p),weights))
-print(target_p)
+print('-'*50)
+print(np.concatenate([logistic(np.dot(add_ones(samples),weights)),target],axis=1))
+print('-'*50)
+#print(np.concatenate([logistic(np.dot(add_ones(s_t_n),weights)),target_t],axis=1))
+print(weights)
+#colors = plt.cm.coolwarm(list(target_t.T))
+#plt.close()
+plt.scatter(samples[:,0], samples[:,1], c=target, s=50)
+x1 = np.linspace(samples[:,0].min(),samples[:,0].max(),5)
+x2 = (-weights[2,0] - weights[0,0]*x1) / weights[1,0]
+plt.plot(x1,x2)
+plt.gray()
+plt.show()
+'''

mlearning.py
@@ -3,25 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tools import *
 
-def regress(samples, target, method='linearGD', iterations=20, rate=0.01, precision=6):
+def linreg(samples, target, method='BGD', iterations=20, rate=0.01, precision=6):
 	# regression
 	# linear with gradient decent or exact formula
 	# or logistic
-	# param method: 'logistic',  'linearGD' - gradient descent, 'linearEX' - using formula
+	# param method:  'BGD' - batch gradient descent, 'Norm' - using formula
+	# 'SGD' stocastic gradient descent
 	# first init weights matrix - dimention m + 1 where m is dimention of samples
 	# adding extra element for x0 = 1
 	weights = np.zeros([samples.shape[1] + 1, 1])
 	# add bias column to samples
 	samples = add_ones(samples)
 	# if method linear exact use formula
-	if method=='linearEX':
+	if method=='Norm':
 		weights = np.linalg.inv(np.dot(np.transpose(samples), samples))
 		weights = np.dot(weights, np.transpose(samples))
 		weights = np.dot(weights, target)
-	elif method in ('logistic', 'linearGD'):
+	elif method in ('BDG', 'SGD'):
 		# calculate h theta
 		h = np.dot(samples, weights)
-		rate = float(rate) / samples.shape[0] 
 		# calculate current error
 		err = (1. / samples.shape[0]) * np.dot(np.transpose(h - target), h - target)
 		# perform decent
@@ -31,7 +31,7 @@ def regress(samples, target, method='linearGD', iterations=20, rate=0.01, precis
 			# save old error
 			old_err = err
 			# calculate gradient
-			grad = np.dot(np.transpose(h - target), samples) * rate
+			grad = np.dot(np.transpose(h - target), samples) * float(rate) / samples.shape[0]
 			# adjust weights
 			weights = weights - np.transpose(grad)
 			# calculate h theta
@@ -54,7 +54,7 @@ def regress(samples, target, method='linearGD', iterations=20, rate=0.01, precis
 	return weights
 	
 	
-def logreg(samples, target, iterations=20, rate=0.01, precision=6):
+def logreg(samples, target, method='BGD', iterations=20, rate=0.01, precision=4):
 	# logistic regression
 	# first init weights matrix - dimention m + 1 where m is dimention of samples
 	# adding extra element for x0 = 1
@@ -66,28 +66,29 @@ def logreg(samples, target, iterations=20, rate=0.01, precision=6):
 	h = np.dot(samples, weights)
 	h = logistic(h)
 	# calculate current error
-	err = (1. / samples.shape[0]) * (np.dot(np.log(h).T,target) + np.dot(np.log(1-h).T,(1 - target)))
-	# perform decent
+	err = (-1. / samples.shape[0]) * (np.dot(np.log(h).T,target) + np.dot(np.log(1-h).T,(1 - target)))
+	# perform decent 
+	
 	for i in range(iterations):
 		# plot error
 		plt.scatter(i,err)
 		# save old error
 		old_err = err
 		# calculate gradient
-		grad = np.dot(np.transpose(h - target), samples) * rate
+		grad = np.dot(np.transpose(h - target), samples) * float(rate)
 		# adjust weights
 		weights = weights - np.transpose(grad)
 		# calculate h theta
 		h = np.dot(samples, weights)
 		h = logistic(h)
 		# calculate current error
-		err = (1. / samples.shape[0]) * (np.dot(np.log(h).T,target) + np.dot(np.log(1-h).T,(1 - target)))
+		err = (-1. / samples.shape[0]) * (np.dot(np.log(h).T,target) + np.dot(np.log(1-h).T,(1 - target)))
 		# calculate relativ error
 		relative_err = err / old_err
 		# print results
-		print('%d iteration: error %.4f relative error %.4f'%(i, err, relative_err))
-		if round(relative_err, precision) == 1.:
-			break
+		# print('%d iteration: error %.4f relative error %.4f'%(i, err, relative_err))
+		if round(relative_err[0][0], precision) == 1.:
+			pass #break
 		elif np.abs(old_err[0][0]) < np.abs(err[0][0]):
 			print('Error increasing, try changing learning rate. Currently: %f'%(rate))
 			weights = 0*weights
@@ -96,7 +97,7 @@ def logreg(samples, target, iterations=20, rate=0.01, precision=6):
 			print('Error increased to infinity, try changing learning rate')
 			weights = 0*weights
 			break 
-	# print('relative error: %.6f'%(relative_err)) 
+	print('relative error: %.6f'%(relative_err)) 
 	return weights
 	
 	